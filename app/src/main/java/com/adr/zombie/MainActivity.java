c3a8c822ae16a9058d4c9e7d57f15b9742eca152
gojosatoru0184847-collab <gojosatoru0184847@gmail.com>
2026-01-31 16:44:21 +0700

    Update MainActivity.java

--- app/src/main/java/com/adr/zombie/MainActivity.java
+++ app/src/main/java/com/adr/zombie/MainActivity.java
@@ -1,103 +1,24 @@
-import android.graphics.Color;
-import android.os.Build;
+package com.adr.zombie;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.webkit.ConsoleMessage;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;   // <<< DÒNG THIẾU
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import androidx.appcompat.app.AppCompatActivity;
-package com.example.kipnn;
-
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.WindowManager;
-import android.webkit.ConsoleMessage;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-
-        setContentView(R.layout.activity_main);
-
-        webView = findViewById(R.id.web);
-
-        // Debug JS console
-        WebView.setWebContentsDebuggingEnabled(true);
-
-        // BẮT BUỘC cho WebGL
-        webView.setBackgroundColor(Color.BLACK);
-        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
-
+        WebView webView = new WebView(this);
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
-        s.setDatabaseEnabled(true);
-
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
-        s.setAllowFileAccessFromFileURLs(true);
-        s.setAllowUniversalAccessFromFileURLs(true);
 
-        s.setMediaPlaybackRequiresUserGesture(false);
-        s.setUseWideViewPort(true);
-        s.setLoadWithOverviewMode(true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onConsoleMessage(ConsoleMessage cm) {
-                android.util.Log.d(
-                        "WEBVIEW",
-                        cm.message() + " -- line " + cm.lineNumber()
-                );
-                return true;
-            }
-        });
-
-        // LOAD GAME
         webView.loadUrl("file:///android_asset/index.html");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        webView.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        webView.destroy();
-        super.onDestroy();
+        setContentView(webView);
     }
 }
